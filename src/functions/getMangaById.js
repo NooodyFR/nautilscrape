1436568ef01455280a8d74aa739a713afdde7b2f
NooodyFR <nooodyfr@gmail.com>
2021-08-20 00:27:16 +0200

    could you work please

--- src/functions/getMangaById.js
+++ src/functions/getMangaById.js
@@ -5,7 +5,7 @@ const { JSDOM } = jsdom;
 async function getMangaById(id) {
 
     try {
-        let req = await axios.get(`https://www.nautiljon.com/mangas/${id}.html`, {
+        let req = await axios.get(`https://www.nautiljon.com/mangas/${encodeURI(id)}.html`, {
             headers: {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
             }
